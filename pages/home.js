31121575e61fe726f1abe1fc43276d82c567229e
noorkhadija557 <noorkhadija557@gmail.com>
2026-08-22 12:25:22 +0500

    push

---
+++ pages/home.js
@@ -0,0 +1,8 @@
+const supabaseUrl = "https://xmexfecjjalkhqtrlzzj.supabase.co";
+const supabaseKey = "sb_publishable_MscDQGxX8gej_btcdCaQjA_6qODt-W8";
+
+const { createClient } = supabase;
+
+const client = createClient(supabaseUrl, supabaseKey)
+
+console.log(client);
